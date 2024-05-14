f3f7f2e7c74863d45bce2a885fd02a14f874ec94
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-05-14 12:53:19 -0300

    added exercicio 07

---
+++ Python/exercicio-07/solucao.py
@@ -0,0 +1,10 @@
+def terca_dobro():
+    numero = float(input('Digite um numero: '))
+    dobro = numero * 2
+    terca_parte = numero / 3
+    print(f'O dobro de {numero} é {dobro}')
+    print(f'A terca parte de {numero} é {terca_parte}')
+    input('Pressione Enter para sair ... ')
+
+
+terca_dobro()
