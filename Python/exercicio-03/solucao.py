36e0146222b5c4d1e9fac91696d9bb6ad5ec3526
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-05-14 12:12:57 -0300

    adicionaei exercicio 03 solucao e problema

---
+++ Python/exercicio-03/solucao.py
@@ -0,0 +1,9 @@
+def ola_funcionario():
+    nome = input('Qual o nome do funcionario ?')
+    salario = int(input('Qual o valor do seu salario ?'))
+    mes_trabalhado = input('Qual foi o ultimo mês trabalhado ?')
+    print(f'O funcionário {nome} tem um salario de {salario}R$ no mês {mes_trabalhado}')
+    input('Pressione Enter para sair ...')
+
+
+ola_funcionario()
