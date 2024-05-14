fa38492eea6d9fdf8b962a1b670e92abe2e726e3
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-05-14 13:04:19 -0300

    added exercicio 08

---
+++ Python/exercicio-08/problema.md
@@ -0,0 +1,13 @@
+### Exercicio 08
+
+Desenvolva um programa que leia uma distância em metros e mostre os valores<br>
+relativos em outras medidas.<br>
+Ex:<br>
+Digite uma distância em metros: 185.72<br>
+A distância de 85.7m corresponde a:<br>
+0.18572Km<br>
+1.8572Hm<br>
+18.572Dam<br>
+1857.2dm<br>
+18572.0cm<br>
+185720.0mm<br>
---
+++ Python/exercicio-08/solucao.py
@@ -0,0 +1,13 @@
+def calcular_medidas():
+    metros = float(input('Digite uma medida em metros: '))
+    kilometros = metros / 1000
+    hectometros = metros / 100
+    decametros = metros / 10
+    decimetros = metros * 10
+    centimetros = metros * 100
+    milimetros = metros * 1000
+    print(f'A distancia de {metros}m corresponde a: \n {kilometros}Km \n {hectometros}Hm \n {decametros}Dam \n {decimetros}dm \n {centimetros} \n {milimetros}mm')
+    print('Pressione Enter para sair')
+
+
+calcular_medidas()
