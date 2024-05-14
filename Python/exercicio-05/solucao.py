c794a6b7d6cfcb32edf6080b5140ffacce3cd37a
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-05-14 12:44:05 -0300

    added exercicio 06 and fixed exercicio 05

--- Python/exercicio-05/solucao.py
+++ Python/exercicio-05/solucao.py
@@ -1,6 +1,6 @@
 def calcular_media():
-    prova1 = int(input('Qual foi a nota da prova 1 ?'))
-    prova2 = int(input('Qual foi a nota da prova 2 ?'))
+    prova1 = float(input('Qual foi a nota da prova 1 ?'))
+    prova2 = float(input('Qual foi a nota da prova 2 ?'))
     media = (prova1 + prova2) / 2
     print(f'A média entre {prova1} e {prova2} é {media}')
     input('Pressione Enter Para Sair...')
---
+++ Python/exercicio-06/solucao.py
@@ -0,0 +1,10 @@
+def antecessor_sucessor():
+    numero = int(input('Digite um número: '))
+    antecessor = numero - 1
+    sucessor = numero + 1
+    print(f'O antecessor de {numero} é {antecessor}')
+    print(f'O sucessor de {numero} é {sucessor}')
+    input('Pressione Enter para sair ... ')
+
+
+antecessor_sucessor()
