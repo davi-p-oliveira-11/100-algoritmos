278e99daf61e07e3b6a412c9dc9b9f532e8841ac
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-05-16 13:35:40 -0300

    adicionei exercicio 10 em python

---
+++ Python/exercicio-10/solucao.py
@@ -0,0 +1,13 @@
+def tinta_necessaria():
+    altura = float(input('Digite a altura da parede em metros: ' ))
+    largura = float(input('Digite a largura da parede em metros: ' ))
+
+    area = largura * altura
+    quantidade_necessaria = area * 0.5
+
+    print(f'A area da parede a ser pintada equivale a {area} m²')
+    print(f'e a quantidade de tinta necessaria equivale a {quantidade_necessaria} litros.')
+    input("Pressione o Enter para sair ... ")
+
+
+tinta_necessaria()
