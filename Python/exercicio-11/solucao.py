658429ce17f5af4d7405fb4769741939c441c75f
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-05-16 14:02:01 -0300

    adicionei exercicio 11 com solucao em python

---
+++ Python/exercicio-11/solucao.py
@@ -0,0 +1,11 @@
+def calcular_area():
+    valor_a = int(input('Qual o valor de A '))
+    valor_b = int(input('Qual o valor de B '))
+    valor_c = int(input('Qual o valor de C '))
+
+    # bhaskara:  delta = b² - 4ac
+    valor_delta = (valor_b ** 2) - 4 * (valor_a * valor_c)
+    print(f'O valor de delta é igual a {valor_delta}')
+    input("Pressione Enter para sair ... ")
+
+calcular_area()
