b9e08833df6cb71e63ea2560f06c707763b61ca7
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-05-22 19:13:06 -0300

    adicionei mais refatoracao

--- Python/exercicio-07/solucao.py
+++ Python/exercicio-07/solucao.py
@@ -1,10 +1,14 @@
-def terca_dobro():
-    numero = float(input('Digite um numero: '))
-    dobro = numero * 2
-    terca_parte = numero / 3
-    print(f'O dobro de {numero} é {dobro}')
-    print(f'A terca parte de {numero} é {terca_parte}')
-    input('Pressione Enter para sair ... ')
+def main():
+    def terca_dobro():
+        numero = float(input('Digite um numero: '))
+        dobro = numero * 2
+        terca_parte = numero / 3
+        print(f'O dobro de {numero} é {dobro}')
+        print(f'A terca parte de {numero} é {terca_parte}')
+    
 
 
-terca_dobro()
+    terca_dobro()
+
+main()
+input('Pressione Enter para sair ... ')
