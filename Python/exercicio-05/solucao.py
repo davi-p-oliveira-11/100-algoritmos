49cc3d52ac1ab47218476eee411f5bd9bd5b8809
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-05-22 18:50:58 -0300

    adicionei mais refatoração

--- Python/exercicio-05/solucao.py
+++ Python/exercicio-05/solucao.py
@@ -1,9 +1,13 @@
-def calcular_media():
-    prova1 = float(input('Qual foi a nota da prova 1 ?'))
-    prova2 = float(input('Qual foi a nota da prova 2 ?'))
-    media = (prova1 + prova2) / 2
-    print(f'A média entre {prova1} e {prova2} é {media}')
-    input('Pressione Enter Para Sair...')
+def main():
+    def calcular_media():
+        prova1 = float(input('Qual foi a nota da prova 1 ?'))
+        prova2 = float(input('Qual foi a nota da prova 2 ?'))
+        media = (prova1 + prova2) / 2
+        print(f'A média entre {prova1} e {prova2} é {media}')
+    
 
 
-calcular_media()
+    calcular_media()
+
+main()
+input('Pressione Enter Para Sair ... ')
