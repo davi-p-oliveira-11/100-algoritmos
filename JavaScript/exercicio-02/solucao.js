6ca69b67f17c77ca4d7910e86e0ee42cac641086
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-05-22 01:19:40 -0300

    encapsulei exercicio 02 em uma função main

--- JavaScript/exercicio-02/solucao.js
+++ JavaScript/exercicio-02/solucao.js
@@ -2,11 +2,18 @@
 const prompt = require("prompt-sync")();
 const readline = require("readline-sync");
 
-function dizerOla() {
-  const nomePessoa = prompt("Qual é o seu nome ?");
+function main() {
+  
+  function dizerOla() {
 
-  console.log(`Olá ${nomePessoa}, é um prazer te conhecer !`);
-  readline.question('Pressione Enter para sair');
+    const nomePessoa = prompt("Qual é o seu nome ? ");
+    console.log(`Olá ${nomePessoa}, é um prazer te conhecer !`);
+    
+  }
+  
+  dizerOla();
 }
 
-dizerOla();
+
+main();
+readline.question('Pressione Enter para sair ... ');
