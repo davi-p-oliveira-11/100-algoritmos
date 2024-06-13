9da36067e6f5a4c66d45bb624146cda3e6e6d797
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-06-13 16:00:35 -0300

    added more ts solutions

---
+++ TypeScript/exercicios/exercicio06/solution06.ts
@@ -0,0 +1,24 @@
+import { prompt, pressEnterToExit } from "../../inputUtils";
+
+function solution06() {
+
+    function antecessorSucessor() {
+
+      let number : number;
+      let antecessor : number;
+      let sucessor : number;
+
+      number = parseInt(prompt("Digite um número: " ));
+      antecessor = number - 1;
+      sucessor = number + 1;
+
+      console.log(`O antecessor de ${number} é ${antecessor} O sucessor de ${number} é ${sucessor}`);
+
+    }
+
+    antecessorSucessor();
+
+}
+
+solution06();
+pressEnterToExit();
