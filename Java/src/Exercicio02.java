77141feeae42db58a704b17bea31fcd147aab19e
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-06-16 20:18:54 -0300

    added new gitignore files

--- Java/src/Exercicio02.java
+++ Java/src/Exercicio02.java
@@ -8,9 +8,7 @@ Qual é o seu nome? João da Silva</br>
 Olá João da Silva, é um prazer te conhecer!</br>
 */
 
-
 import java.util.Scanner;
-import java.util.InputMismatchException;
 import java.util.Locale;
 
 public class Exercicio02 {
@@ -24,23 +22,8 @@ public class Exercicio02 {
     static void DizerOla() {
       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-      String nome = "";
-      boolean entradaValida = false;
-
-      while (!entradaValida) {
-          try {
-              System.out.print("Digite seu nome: ");
-              nome = scanner.next();
-         
-              if (!nome.matches("[a-zA-Z]+")) {
-                  throw new InputMismatchException("Entrada inválida. Por favor, digite apenas letras.");
-              }
-              entradaValida = true; 
-          } catch (InputMismatchException e) {
-              System.out.println("Entrada inválida. Por favor, digite apenas letras.");
-              scanner.next(); 
-          }
-      }
+      System.out.print("Digite seu nome: ");
+      String nome = scanner.next();
 
       System.out.println("Olá " + nome + ", é um prazer te conhecer!");
       scanner.close();
