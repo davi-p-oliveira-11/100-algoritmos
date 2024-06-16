bc1fee6fa1965003c05e618fc4c86d7da81090d6
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-06-16 18:05:12 -0300

    added first java problem/solution

---
+++ Java/src/Exercicio01.java
@@ -0,0 +1,13 @@
+/* Exercicio 01:
+    Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+*/ 
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Olá mundo !");
+
+    }
+  
+}
