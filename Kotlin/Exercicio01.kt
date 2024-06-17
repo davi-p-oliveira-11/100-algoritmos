e93b4295145b76a200da88ed795e4531f32ad59c
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-06-17 19:21:24 -0300

    addded Kotlin folder

---
+++ Kotlin/Exercicio01.kt
@@ -0,0 +1,17 @@
+/*
+ ### Exercicio 01:
+
+ Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+
+ */
+
+ fun main() {
+
+    fun SayHello() {
+
+      println("Hello World ! ");
+
+    }
+
+    SayHello();
+ }
