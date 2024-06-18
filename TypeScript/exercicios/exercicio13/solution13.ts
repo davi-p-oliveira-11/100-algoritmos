b077cab76cfbaf7e68ff11eff74aae0037f44a90
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-06-18 14:06:35 -0300

    added solution13 in TS

---
+++ TypeScript/exercicios/exercicio13/solution13.ts
@@ -0,0 +1,30 @@
+/*
+### Exercicio 13
+
+Faça um algoritmo que leia o salário de um funcionário,<br> calcule e mostre o
+seu novo salário, com 15% de aumento.
+*/
+import { prompt, pressEnterToExit } from "../../inputUtils";
+
+function solution13() {
+   
+    function aumentoSalario() {
+       
+      let valorSalario : number;
+      let calcularAumento : number;
+      let novoSalario : number;
+
+      valorSalario = parseFloat(prompt('Qual o valor do seu salário ?'));
+
+      calcularAumento = valorSalario * 0.15;
+      novoSalario = valorSalario + calcularAumento;
+
+      console.log(`O seu novo salário com 15% de aumento equivale a ${novoSalario}`);
+ 
+    }
+
+    aumentoSalario();
+}
+
+solution13();
+pressEnterToExit();
