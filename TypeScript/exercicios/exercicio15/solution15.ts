d5bee3d9d3fbb2c2167e187b3f769d7c82eb29f8
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-06-18 15:36:56 -0300

    added solution15TS

---
+++ TypeScript/exercicios/exercicio15/solution15.ts
@@ -0,0 +1,30 @@
+/* 
+  15) Crie um programa que leia o número de dias trabalhados em um mês e mostre o
+  salário de um funcionário, sabendo que ele trabalha 8 horas por dia e ganha R$25
+  por hora trabalhada. 
+*/
+import { prompt, pressEnterToExit } from "../../inputUtils";
+
+function solution15() {
+
+    function mostrarSalario() {
+
+      let diasTrabalhados : number;
+      let diaDeTrabalho : number;
+      let salarioFuncionario : number;
+
+      diasTrabalhados = parseInt(prompt('Quantos dias voce trabalhou no último mes ?'));
+      diaDeTrabalho = 8 * 25;
+      salarioFuncionario = diasTrabalhados * diaDeTrabalho;
+      
+      console.log(`O funcionário trabalhou ${diasTrabalhados} dias
+      e vai receber um salario de ${salarioFuncionario} reais`);
+
+    }
+    
+    mostrarSalario();
+
+}
+
+solution15();
+pressEnterToExit();
