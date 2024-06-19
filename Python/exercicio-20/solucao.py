6a63a343ee7fcb1c3db82b394b70bed31b6b1019
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-06-19 11:57:54 -0300

    added exercicio20 in python

---
+++ Python/exercicio-20/solucao.py
@@ -0,0 +1,9 @@
+'''
+20) Desenvolva um programa que leia um número inteiro e mostre se ele é PAR ou
+ÍMPAR 
+'''
+
+numero = int(input("Digite um número inteiro: "))
+
+print("Numero par") if numero % 2 == 0 else print("O numero é impar")
+
