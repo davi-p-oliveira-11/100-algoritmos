36d5bfeb4db508bb5879219e04d58ff84c20265e
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-07-15 20:03:52 -0300

    updated first java solution

---
+++ Java/src/HelloWorld01.java
@@ -0,0 +1,11 @@
+/* 1 - Write a program that displays the message "Hello, World!" on the screen. */
+
+public class HelloWorld01 {
+
+  public static String HelloWorld(String message) {
+    return message;
+  }
+  public static void main(String[] args) {
+    System.out.println(HelloWorld("Hello, World !"));
+  }
+}
